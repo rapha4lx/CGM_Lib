0ef7a66ad0320b8b6bf7f97c499c7e8325224665
rapha4lx <rapha4.lx@gmail.com>
2025-08-15 14:02:04 -0300

    Refact: Renamed folder and removed raise to return error

--- json/json.py
+++ json/json.py
@@ -4,9 +4,8 @@ import os
 
 def load_json(path='emails.json'):
     if not os.path.exists(path):
-        raise FileNotFoundError(f"File not found: {path}")
+        return None
 
     with open(path, 'r', encoding='utf-8') as file:
         emails_data = json.load(file)
-        #emails_data = emails_data.get("default", [])
         return emails_data
